5dac28783b084b8fc8d8c337ffd35fc2bc5d355e
Matt <mattj456@users.noreply.github.com>
2017-12-04 01:45:59 -0800

    im an idiot

--- app.js
+++ app.js
@@ -72,7 +72,6 @@ function calcTime(city, offset) {
 	var dayWeek = [[0, "Sunday"], [1, "Monday"], [2, "Tuesday"], [3, "Wednesday"], [4, "Thursday"], [5, "Friday"], [6, "Saturday"]];
 
 	shortTime = Number(fullHour.toString() + pad(nd.getMinutes()).toString());
-	shortTime = 700;
 
 	wkday = dayWeek[nd.getDay()][1];
 	
